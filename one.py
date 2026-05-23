872da67473483b858c93a9be407eb2dd80ef9eec
wangucodes <wangunjovu@gmail.com>
2026-05-23 17:44:05 +0200

    Add files via upload

---
+++ one.py
@@ -0,0 +1,9 @@
+#printing a greeting message
+print("Hello world!")
+#printing my name
+print("my name is Wangu")
+#printing a fun fact about myself
+#printing ("I love playing chess")
+print("I love playing chess")
+#performing and printing a simple calculation
+print("the result of 3 + 5 is;", 3 + 5)
