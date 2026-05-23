a772f45e1f3be2308a08c1e61bf2c3d3f43738d9
wangucodes <wangunjovu@gmail.com>
2026-05-23 17:52:42 +0200

    Add files via upload

---
+++ two.py
@@ -0,0 +1,10 @@
+#printing a sentence
+print("I enjoy reading in my free time.")
+#printing a anumber
+print(4)
+#printing a line after a sentence
+print("hello python \n")
+#printing all keywords present in python
+print("Keywords present in python are...")
+import keyword
+print(keyword.kwlist)
