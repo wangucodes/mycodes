1d9119eb077ca3581760a20213e1b5494da0677e
wangucodes <wangunjovu@gmail.com>
2026-05-24 17:46:01 +0200

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,18 @@
+name="Wangu"
+age=16
+is_student=True
+weight=56.8
+print("name :",name)
+print("Data type of name is", type(name))
+print("age :",age)
+print("Data type of age is" ,type(age))
+print("is_student" ,is_student)
+print("Data type of is_student is", type(is_student))
+print("weight :", weight)
+print("Data type of weight is" ,type(weight))
+print("\n After Type Casting...")
+age = str(age)
+print(age)
+print("Data type of age is", type(age))
+weight- int(weight)
+print("Data type of weight is ", type(weight))
