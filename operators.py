569009a299dc3bdb7471e4a49fc025176a38b686
wangucodes <wangunjovu@gmail.com>
2026-05-24 18:19:35 +0200

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,18 @@
+num1=2
+num2=45
+print("Number 1 :", num1)
+print("Number 2 :", num2)
+print("Addition :", num1+num2)
+print("Difference :", num2-num1)
+print("Division :", num2/num1)
+print("Product :", num1*num2)
+print("Floor division :", num2//num1)
+print("Module operation :", num1%num2)
+print("Square :",num1**num2)
+print("Square root :", num1**0.5)
+print("Equal ?", num1==num2)
+print("Number 1 greater?", num1<num2)
+print("Number 2 greater?", num1>num2)
+print("Not Equal?", num1 !=num2)
+result = num1/2 + num2**3+10
+print("Result of given equation is:", result)
