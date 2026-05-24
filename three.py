f0976efe946694027bb02dcf4110196704c76470
wangucodes <wangunjovu@gmail.com>
2026-05-24 18:21:00 +0200

    Add files via upload

---
+++ three.py
@@ -0,0 +1,13 @@
+First_name="Codingal"
+Last_name="Education"
+Full_name="First_name+Last_name"
+example = "haa"*5
+print("First_name", First_name)
+print("Last_name", Last_name)
+print("Full_name", Full_name)
+print("string multiplied 5 times gives this result :", example)
+word='Coding'
+print("length of string :", len(word))
+print("First letter of word :", word[0])
+print("Last letter of word :", word[5])
+print("String sliced :", word[0:3])
