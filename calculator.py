9c36d8f39078bc800cd69d6b99cddaf266934232
wangucodes <wangunjovu@gmail.com>
2026-06-13 18:05:55 +0200

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,14 @@
+def add(x,y):
+    return x + y
+def subtract(x,y):
+    return x - y
+def multiply(x,y):
+    return x * y
+def divide(x,y):
+    return x / y
+num1 = int(input("Enter number 1"))
+num2 = int(input("Enter number 2"))
+print("sum :", add(num1, num2))
+print("Difference :", subtract(num1, num2))
+print("product :", multiply(num1 ,num2))
+print("quotient :", divide(num1 ,num2))
