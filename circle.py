0ced12153af2e1f8a6d3f2fc599bc6be2fb06c4e
wangucodes <wangunjovu@gmail.com>
2026-07-26 02:44:09 +0200

    Add files via upload

---
+++ circle.py
@@ -0,0 +1,27 @@
+import turtle
+screen = turtle.screen()
+screen.bgcolor("black")
+screen.title("colour loop artwork")
+artist = turtle.Turtle
+artist.speed("fastest")
+artist.hideturtle()
+artist.pensize(2)
+def draw_petal(size, colour):
+    artist.color(colour)
+    artist.begin_fill()
+    for _ in range(2):
+        artist.circle(size,60)
+        artist.left(120)
+    artist.end_fill()
+colours = ["Red", "Blue", "pink", "purple", "yellow", "orange", "green"]
+for i in range(36):
+    draw_petal(90, colours[i % len(colours)])
+    artist.right(10)
+artist.penup()
+artist.goto(0, -25)
+artist.pendown()
+artist.color("white")
+artist.begin_fill()
+artist.circle(25)
+artist.end_fill()
+turtle.done()
